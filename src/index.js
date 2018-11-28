1d2882a9f6ec6f976ebf001a4ed86dde1c8abec8
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-11-28 02:09:20 -0600

    updated index files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import 'bootstrap';
 import firebase from 'firebase/app';
-
+// import $ from 'jquery';
 import apiKeys from '../db/apiKeys.json';
 
 import './index.scss';
-
+import tasks from './components/TasksPage/tasksPage';
 import navbar from './components/Navbar/navbar';
 import loginButton from './components/Auth/auth';
 import checkLoginStatus from './components/helpers/authHelpers';
+
 
 const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   navbar();
   checkLoginStatus();
   loginButton();
+  tasks.getTasks();
+  tasks.initView();
 };
 
 initializeApp();
